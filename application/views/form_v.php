d87f7295b0c3eb71262759b7606eea2a1aa8e463
Endromuhammad <endropujiantoro1@gmail.com>
2018-05-07 07:35:26 +0700

    inputuser

---
+++ application/views/form_v.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<title>Hello</title>
+</head>
+<body>
+	<form action="http://localhost/travelloh/index.php/User/Add" method="post">
+		username :<input type="text" name ="username"></br>
+		fullname :<input type="text" name ="fullname"></br>
+		password :<input type="password" name ="password"></br>
+		level <select name ="level">
+		<option name ="Admin"> Admin </option>
+		<option name ="User"> user </option>
+	</select>
+		<button type ="submit">Register</button></br>
+	</form>
+</body>
+</html>
